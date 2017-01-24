b9ff1eb3e131c37493da4fb01974288da8415a3e
alecmerdler <merdlera@oregonstate.edu>
2017-01-24 15:18:43 -0800

    type definitions for binary tree

--- assn1/Tree.merdlera.hs
+++ assn1/Tree.merdlera.hs
@@ -56,7 +56,9 @@ leftmost (Node _ l _) = leftmost l
 --   >>> rightmost t2
 --   9
 --
-rightmost = undefined
+rightmost :: Tree -> Int
+rightmost (Leaf i)     = i
+rightmost (Node _ _ r) = rightmost r
 
 
 -- | Get the maximum integer from a binary tree.
@@ -76,7 +78,9 @@ rightmost = undefined
 --   >>> maxInt t2
 --   9
 --
-maxInt = undefined
+maxInt :: Tree -> Int
+maxInt (Leaf i) = i
+--maxInt (Node i l r) = (maxInt l) (maxInt r)
 
 
 -- | Get the minimum integer from a binary tree.
@@ -96,7 +100,8 @@ maxInt = undefined
 --   >>> minInt t2
 --   1
 --
-minInt = undefined
+minInt :: Tree -> Int
+minInt (Leaf i) = i
 
 
 -- | Get the sum of the integers in a binary tree.
@@ -113,7 +118,8 @@ minInt = undefined
 --   >>> sumInts (Node 10 t1 t2)
 --   100
 --
-sumInts = undefined
+sumInts :: Tree -> Int
+sumInts (Leaf i) = i
 
 
 -- | The list of integers encountered by a pre-order traversal of the tree.
@@ -130,7 +136,8 @@ sumInts = undefined
 --   >>> preorder t2
 --   [6,2,1,4,3,5,8,7,9]
 --
-preorder = undefined
+preorder :: Tree -> [Int]
+preorder (Leaf i) = [i]
 
 
 -- | The list of integers encountered by an in-order traversal of the tree.
@@ -147,7 +154,8 @@ preorder = undefined
 --   >>> inorder t2
 --   [1,2,3,4,5,6,7,8,9]
 --
-inorder = undefined
+inorder :: Tree -> [Int]
+inorder (Leaf i) = [i]
 
 
 -- | Check whether a binary tree is a binary search tree.
@@ -164,7 +172,8 @@ inorder = undefined
 --   >>> isBST t2
 --   True
 --
-isBST = undefined
+isBST :: Tree -> Bool
+isBST (Leaf i) = True
 
 
 -- | Check whether a number is contained in a binary search tree.
@@ -182,4 +191,7 @@ isBST = undefined
 --   >>> inBST 10 t2
 --   False
 --
-inBST = undefined
+inBST :: Int -> Tree -> Bool
+inBST a (Leaf i)
+    | a == i = True
+    | otherwise = False

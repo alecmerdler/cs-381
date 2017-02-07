82d654a122eca6c6635aac4fa195e60904c5d1d7
alecmerdler <merdlera@oregonstate.edu>
2017-02-07 13:52:00 -0800

    helper function to convert from (State, Line) to (State, [Line])

--- assn3/HW3.merdlera.hs
+++ assn3/HW3.merdlera.hs
@@ -14,5 +14,35 @@ type State = (Mode, Int, Int)
 -- >>> cmd (Pen Up) (Down, 0, 0)
 -- ((Up,0,0),((0,0),(0,0)))
 --
+-- >>> cmd (Move 1 1) (Down, 0, 0)
+-- ((Down,1,1),((0,0),(1,1)))
+--
 cmd :: Cmd -> State -> (State, Line)
-cmd (Pen a) (b, x, y) = ((a, x, y), ((0, 0), (0, 0)))
+cmd (Pen a) (_, x, y)        = ((a, x, y), ((0, 0), (0, 0)))
+cmd (Move x2 y2) (b, x1, y1) = ((b, x2, y2), ((x1, y1), (x2, y2)))
+
+
+--
+-- | Part II: Implement prog, the semantic function for MiniMiniLogo programs (Prog).
+--
+-- >>> prog [] (Up, 0, 4)
+-- ((Up,0,4),[((0,0),(0,0))])
+--
+-- >>> prog [(Pen Up)] (Down, 0, 0)
+-- ((Up,0,0),[((0,0),(0,0))])
+--
+prog :: Prog -> State -> (State, [Line])
+prog [] (a, x, y) = ((a, x, y), [((0, 0), (0, 0))])
+prog [a] b = composeCmd (cmd a b)
+prog (a:bs) (c, x, y) = undefined
+
+
+--
+-- * Helper Functions
+--
+
+--
+-- |
+--
+composeCmd :: (State, Line) -> (State, [Line])
+composeCmd (a, b) = (a, [b])

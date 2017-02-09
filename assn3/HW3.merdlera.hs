264f07f82413a4a7414f66976024de60002b1f51
alecmerdler <merdlera@oregonstate.edu>
2017-02-09 01:38:17 -0800

    stashing

--- assn3/HW3.merdlera.hs
+++ assn3/HW3.merdlera.hs
@@ -7,9 +7,6 @@ import MiniMiniLogo
 -- | Represents the state of the pen
 type State = (Mode, Int, Int)
 
--- | Represents no line drawn
-noLine = ((0, 0), (0, 0))
-
 
 --
 -- | Part I: Implement cmd, the semantic function for MiniMiniLogo commands (Cmd).
@@ -28,24 +25,24 @@ cmd (Move x2 y2) (b, x1, y1) = ((b, x2, y2), Just ((x1, y1), (x2, y2)))
 --
 -- | Part II: Implement prog, the semantic function for MiniMiniLogo programs (Prog).
 --
--- >> prog [] (Up, 0, 4)
--- ((Up,0,4),[((0,0),(0,0))])
+-- >>> prog [] (Up, 0, 4)
+-- ((Up,0,4),[Nothing])
 --
--- >> prog [(Pen Up)] (Down, 0, 0)
--- ((Up,0,0),[((0,0),(0,0))])
+-- >>> prog [(Pen Up)] (Down, 0, 0)
+-- ((Up,0,0),[Nothing])
 --
--- >> prog [(Move 1 1)] (Down, 0, 0)
--- ((Down,1,1),[((0,0),(1,1))])
+-- >>> prog [(Move 1 1)] (Down, 0, 0)
+-- ((Down,1,1),[Just ((0,0),(1,1))])
 --
 -- >> prog [(Move 1 2), (Pen Up)] (Down, 0, 0)
--- ((Up,1,2),[((0,0),(1,2))])
+-- ((Up,1,2),[Just ((0,0),(1,2))])
 --
 -- >> prog [(Move 1 2), (Move 1 3)] (Down 0 0)
--- ((Down,1,3),[((0,0),(1,2)),((1,2),(1,3))]
+-- ((Down,1,3),[Just ((0,0),(1,2)),Just ((1,2),(1,3))]
 --
---prog :: Prog -> State -> (State, [Line])
---prog [] state          = (state, [])
---prog (head:tail) state = prog tail ((\(newState, line) -> (newState, [line])) (cmd head state))
+prog :: Prog -> State -> (State, [Maybe Line])
+prog [] state       = (state, [Nothing])
+prog (c:cmds) state = applyCmd c (state, [])
 
 
 --
@@ -62,5 +59,5 @@ cmd (Move x2 y2) (b, x1, y1) = ((b, x2, y2), Just ((x1, y1), (x2, y2)))
 -- ((Down,1,1),[Just ((0,0),(1,1)),Just ((0,0),(2,2))])
 --
 applyCmd :: Cmd -> (State, [Maybe Line]) -> (State, [Maybe Line])
-applyCmd a (state, [])      = (\(newState, line) -> (newState, [line])) (cmd a state)
-applyCmd a (state, lines) = (\(newState, line) -> (newState, line : lines)) (cmd a state)
+applyCmd newCmd (state, [])    = (\ (newState, line) -> (newState, [line])) (cmd newCmd state)
+applyCmd newCmd (state, lines) = (\ (newState, line) -> (newState, line : lines)) (cmd newCmd state)

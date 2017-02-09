10958143281b38a5eb8ac677c6d5c25b3a987112
alecmerdler <merdlera@oregonstate.edu>
2017-02-08 21:24:44 -0800

    applyCmd helper function

--- assn3/HW3.merdlera.hs
+++ assn3/HW3.merdlera.hs
@@ -1,4 +1,4 @@
-module HW3 where
+module HW3 (cmd) where
 
 import Render
 import MiniMiniLogo
@@ -28,26 +28,39 @@ cmd (Move x2 y2) (b, x1, y1) = ((b, x2, y2), Just ((x1, y1), (x2, y2)))
 --
 -- | Part II: Implement prog, the semantic function for MiniMiniLogo programs (Prog).
 --
--- >>> prog [] (Up, 0, 4)
+-- >> prog [] (Up, 0, 4)
 -- ((Up,0,4),[((0,0),(0,0))])
 --
--- >>> prog [(Pen Up)] (Down, 0, 0)
+-- >> prog [(Pen Up)] (Down, 0, 0)
 -- ((Up,0,0),[((0,0),(0,0))])
 --
--- >>> prog [(Move 1 1)] (Down, 0, 0)
+-- >> prog [(Move 1 1)] (Down, 0, 0)
 -- ((Down,1,1),[((0,0),(1,1))])
 --
--- >>> prog [(Move 1 2), (Pen Up)] (Down, 0, 0)
+-- >> prog [(Move 1 2), (Pen Up)] (Down, 0, 0)
 -- ((Up,1,2),[((0,0),(1,2))])
 --
--- >>> prog [(Move 1 2), (Move 1 3)] (Down 0 0)
+-- >> prog [(Move 1 2), (Move 1 3)] (Down 0 0)
 -- ((Down,1,3),[((0,0),(1,2)),((1,2),(1,3))]
 --
-prog :: Prog -> State -> (State, [Line])
-prog [] state          = (state, [])
-prog (head:tail) state = prog tail ((\(newState, line) -> (newState, [line])) (cmd head state))
-prog = undefined
+--prog :: Prog -> State -> (State, [Line])
+--prog [] state          = (state, [])
+--prog (head:tail) state = prog tail ((\(newState, line) -> (newState, [line])) (cmd head state))
+
 
 --
 -- * Helper Functions
 --
+
+--
+-- | Applies a given command to an existing state and set of lines
+--
+-- >>> applyCmd (Pen Down) ((Up, 0, 0), [])
+-- ((Down,0,0),[Nothing])
+--
+-- >>> applyCmd (Move 1 1) ((Down, 0, 0), [Just ((0, 0), (2, 2))])
+-- ((Down,1,1),[Just ((0,0),(1,1)),Just ((0,0),(2,2))])
+--
+applyCmd :: Cmd -> (State, [Maybe Line]) -> (State, [Maybe Line])
+applyCmd a (state, [])      = (\(newState, line) -> (newState, [line])) (cmd a state)
+applyCmd a (state, lines) = (\(newState, line) -> (newState, line : lines)) (cmd a state)
